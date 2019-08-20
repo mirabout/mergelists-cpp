6c282c58d4f150881df017584e79aeb4cbbddf18
mirabout <mirabout@users.noreply.github.com>
2019-08-20 13:06:28 +0300

    Actually use the JSON library for parsing/output
    Also downgrade C++ version to C++14 as there's no
    full support for C++17 headers on the dev. machine.

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
-#include <iostream>
 #include <algorithm>
+#include <fstream>
+#include <iostream>
 #include <string>
 #include <vector>
 #include <unordered_map>
+
+#include <nlohmann/json.hpp>
 
 struct Entry {
 	std::string title;
@@ -62,8 +65,8 @@ void MergeBuilder::addEntries( const std::vector<Entry> &entries ) {
 
 std::vector<const Entry *> MergeBuilder::build() {
 	std::vector<const Entry *> result;
-	for( const auto &[key, ref] : buckets ) {
-		result.push_back( ref );
+	for( const auto &kvPair : buckets ) {
+		result.push_back( kvPair.second );
 	}
 	// Provide a proper comparator for sorting pointers to items
 	auto cmp = []( const Entry *lhs, const Entry *rhs ) { return *lhs < *rhs; };
@@ -71,11 +74,110 @@ std::vector<const Entry *> MergeBuilder::build() {
 	return result;
 }
 
-static bool tryReadingEntries( const char *filename, std::vector<Entry> &output ) {
+// Wrap keys once for faster access in the parsing loop
+static std::string FIELD_NUM( "num" );
+static std::string FIELD_TITLE( "title" );
+static std::string FIELD_CREATED( "created" );
+static std::string FIELD_DELETED( "deleted" );
+
+template <typename T>
+static bool getField( const nlohmann::json &json, const std::string &key, T &result, std::string &error ) {
+	auto it = json.find( key );
+	if( it != json.end() ) {
+		result = *it;
+		return true;
+	}
+	error = std::string( "Failed to get field `" ) + key + "` of an entry";
 	return false;
 }
 
+template <typename T>
+static bool tryGettingField( const nlohmann::json &json, const std::string &key, T &result ) {
+	auto it = json.find( key );
+	if( it != json.end() ) {
+		result = *it;
+		return true;
+	}
+	return false;
+}
+
+static bool tryParsingEntries( const nlohmann::json &root, std::vector<Entry> &output, std::string &error ) {
+	if( !root.is_array() ) {
+		error = "The root JSON object is not an array";
+		return false;
+	}
+
+	std::vector<Entry> result;
+	for( const nlohmann::json &elem: root ) {
+		if( !elem.is_object() ) {
+			error = "An element of a root JSON array is not an object";
+			return false;
+		}
+		Entry entry;
+		if( !::getField( elem, FIELD_NUM, entry.num, error ) ) {
+			return false;
+		}
+		if( !::getField( elem, FIELD_TITLE, entry.title, error ) ) {
+			return false;
+		}
+		assert( entry.created == 0 && entry.deleted == 0 );
+		const bool hasCreated = ::tryGettingField( elem, FIELD_CREATED, entry.created );
+		const bool hasDeleted = ::tryGettingField( elem, FIELD_DELETED, entry.deleted );
+		if( hasCreated && hasDeleted ) {
+			error = "Both `created` and `deleted` fields are present";
+			return false;
+		}
+		if( !( hasCreated || hasDeleted ) ) {
+			error = "Both `created` and `deleted` fields are absent";
+			return false;
+		}
+		if( hasCreated ) {
+			entry.timestamp = entry.created;
+		}
+		if( hasDeleted ) {
+			entry.timestamp = entry.deleted;
+		}
+		result.emplace_back( std::move( entry ) );
+	}
+
+	// There's nothing that could throw left. Commit changes.
+	output.clear();
+	std::swap( result, output );
+	return true;
+}
+
+static bool tryReadingEntries( const char *filename, std::vector<Entry> &output, std::string &error ) {
+	try {
+		std::ifstream stream;
+		stream.open( filename, std::ios_base::in );
+		if( !stream.is_open() ) {
+			error = "Failed to open a file stream";
+			return false;
+		}
+		nlohmann::json root( nlohmann::json::parse( stream ) );
+		return tryParsingEntries( root, output, error );
+	} catch( std::exception &ex ) {
+		error = ex.what();
+		return false;
+	}
+}
+
 static void printEntries( const std::vector<const Entry *> &entries ) {
+	nlohmann::json root;
+	for( const Entry *entry : entries ) {
+		nlohmann::json obj;
+		obj[FIELD_NUM] = entry->num;
+		obj[FIELD_TITLE] = entry->title;
+		if( entry->created ) {
+			obj[FIELD_CREATED] = entry->created;
+		}
+		if( entry->deleted ) {
+			obj[FIELD_DELETED] = entry->deleted;
+		}
+		root.emplace_back( std::move( obj ) );
+	}
+	// Dump to the std::cout using pretty-printing
+	std::cout << root.dump(2) << std::endl;
 }
 
 int main( int argc, char **argv ) {
@@ -87,10 +189,12 @@ int main( int argc, char **argv ) {
 	// Content of all files is read first and is kept at a permanent address during the MergeBuilder lifetime.
 	// The MergeBuilder operates on raw pointers to entries that are assumed to be owned by something else.
 	std::vector<std::vector<Entry>> readLists;
+
+	std::string error;
 	for( int i = 1; i < argc; ++i ) {
 		std::vector<Entry> content;
-		if( !::tryReadingEntries( argv[i], content ) ) {
-			std::cerr << "Failed to read a file content of `" << argv[i] << " `" << std::endl;
+		if( !::tryReadingEntries( argv[i], content, error ) ) {
+			std::cerr << "Failed to read a file content of `" << argv[i] << " `: " << error << std::endl;
 			return 1;
 		}
 		readLists.emplace_back( std::move( content ) );
